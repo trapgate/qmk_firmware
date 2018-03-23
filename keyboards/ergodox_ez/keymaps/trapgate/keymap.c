796dfbaff1b412197a51817d47ef0565d69cf8f8
Geoff Hickey <trapgate@gmail.com>
2018-03-22 22:46:30 -0400

    ergodox-ez:trapgate: fix layer 1 toggle.

--- keyboards/ergodox_ez/keymaps/trapgate/keymap.c
+++ keyboards/ergodox_ez/keymaps/trapgate/keymap.c
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        VRSN,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_ESC,
        KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
        KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,TG(MDIA),
+       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,TG(GAME),
           EPRM,KC_TRNS,KC_TRNS,KC_LEFT,KC_RIGHT,
                                        RGB_MOD,KC_TRNS,
                                                KC_TRNS,
