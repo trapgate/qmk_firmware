ddc75781732b958759e8ea067d04091cc6aab73a
Geoff Hickey <trapgate@gmail.com>
2018-08-12 18:35:18 -0400

    small changes to the game and symbol layers

--- keyboards/ergodox_ez/keymaps/trapgate/keymap.c
+++ keyboards/ergodox_ez/keymaps/trapgate/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   _______,
         KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_ENT,
-        KC_LCTL,        KC_QUOT, LALT(KC_LSFT), KC_LEFT, KC_RGHT,
+        KC_LCTL,        KC_LALT,      MO(SYMB),       KC_LEFT,        KC_RGHT,
                                               ALT_T(KC_NO),   KC_LGUI,
                                                               KC_HOME,
                                                 KC_SPC,KC_ENT,KC_END,
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|  L2  |           |      |------+------+------+------+------+--------|
  * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | EPRM  |      |      |      |      |                                       |   0  |   .  |      |   =  |      |
+ *   |       |      |      |      |      |                                       |   0  |   ,  |   .  |   =  |      |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -118,17 +118,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        VRSN,   KC_F1,     KC_F2,      KC_F3,      KC_F4,    KC_F5,    KC_ESC,
        _______,KC_EXLM,   KC_AT,      KC_LCBR,    KC_RCBR,  KC_PIPE,  _______,
        _______,KC_HASH,   KC_DLR,     KC_LPRN,    KC_RPRN,  KC_GRV,
-       _______,CTL_T(KC_PERC),ALT_T(KC_CIRC),KC_LBRC,KC_RBRC,KC_TILD, _______,
-          EPRM,_______,_______,KC_LEFT,KC_RIGHT,
+       _______,CTL_T(KC_PERC), ALT_T(KC_CIRC),KC_LBRC,KC_RBRC,KC_TILD, _______,
+       _______,_______,_______,KC_LEFT,KC_RIGHT,
                                            _______, _______,
                                                     _______,
                                   _______, _______, _______,
        // right hand
-       _______, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       _______,KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        _______, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, _______,
        _______, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, _______,
-                         KC_0,   KC_DOT,  _______, KC_EQL,  _______,
+                         KC_0,   KC_COMM, KC_DOT,  KC_EQL,  _______,
        _______, _______,
        _______,
        _______, _______, KC_WBAK
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |RESET |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                _______,
                                RGB_VAD,RGB_VAI,_______,
     // right hand
-       _______,  _______, _______, _______, _______, _______, _______,
+       RESET,    _______, _______, _______, _______, _______, _______,
        _______,  _______, _______, _______, _______, _______, _______,
                  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, KC_MPLY,
        _______,  _______, _______, KC_MPRV, KC_MNXT, _______, _______,
