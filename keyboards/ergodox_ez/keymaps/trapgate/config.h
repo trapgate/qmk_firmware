70c31344f00bc618c789a740c1b6bb8c65c934bd
Geoff Hickey <trapgate@gmail.com>
2020-06-06 03:03:57 -0400

    cleanup ergodox_ez:trapgate layout
    - switch to newer LAYOUT_ergodox_pretty macro
    - remove some obsolete definitions
    - turn on LINK_TIME_OPTIMIZATION_ENABLE

--- keyboards/ergodox_ez/keymaps/trapgate/config.h
+++ keyboards/ergodox_ez/keymaps/trapgate/config.h
@@ -7,12 +7,12 @@
 // From my tests, the options mean the following
 //
 // PERMISSIVE_HOLD = Always the modified output
-// IGNORE_MOD_TAP_INTERRUPT = Always the both keys, unless you hold both keys
+// IGNORE_MOD_TAP_INTERRUPT = Always both keys, unless you hold both keys
 //                            for at least TAPPING_TERM
 // PERMISSIVE_HOLD + IGNORE_MOD_TAP_INTERRUPT = Modified output if the other key
 //     is released before the tap key, or if both keys are held longer than
 //     TAPPING_TERM. So both keys are output when releasing the tap key before
-//     the other key and held lesss than TAPPING_TERM
+//     the other key and held less than TAPPING_TERM
 
 #define PREVENT_STUCK_MODIFIERS
 #define PERMISSIVE_HOLD
@@ -20,9 +20,9 @@
 #define TAPPING_TERM 200
 // #undef IGNORE_MOD_TAP_INTERRUPT
 
-// The default for debounce is now 5, not 15, so this is no longer needed.
+// The default for debounce is now 30
 // #undef DEBOUNCE
-// #define DEBOUNCE 4
+// #define DEBOUNCE 60
 
 // The default value for IS_COMMAND includes both shift keys, and that ends up
 // getting tripped accidentally when holding right shift while hitting symbol
