8ad9908e784d6ab0a706d159358128d647ad8e35
Geoff Hickey <trapgate@gmail.com>
2021-06-16 03:41:12 -0400

    LEDs for SYMB layer, caps lock

--- keyboards/moonlander/keymaps/trapgate/keymap.c
+++ keyboards/moonlander/keymaps/trapgate/keymap.c
@@ -39,10 +39,10 @@ enum dances {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_EQL,         KC_1,            KC_2,         KC_3,   KC_4,   KC_5, KC_ESC,    KC_LOCK,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_MINS,
+        KC_EQL,         KC_1,            KC_2,         KC_3,   KC_4,   KC_5, KC_ESC,    KC_CAPS,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_MINS,
         KC_DELT,        KC_Q,            KC_W,         KC_E,   KC_R,   KC_T, TD(DLAYR), KC_ENT,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
         KC_BSPC,        KC_A,            KC_S,         KC_D,   KC_F,   KC_G, TD(DLFT),  TD(DRGHT), KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-        KC_LSFT,        CTL_T(KC_Z),   ALT_T(KC_X),  KC_C,   KC_V,   KC_B,                       KC_N,   KC_M,   KC_COMM,ALT_T(KC_DOT),CTL_T(KC_SLSH),KC_RSFT,
+        KC_LSFT,        CTL_T(KC_Z),     ALT_T(KC_X),  KC_C,   KC_V,   KC_B,                       KC_N,   KC_M,   KC_COMM,ALT_T(KC_DOT),CTL_T(KC_SLSH),KC_RSFT,
         LT(SYMB,KC_GRV),LT(MDIA,KC_QUOT),LALT(KC_LGUI),KC_LEFT,KC_RGHT,KC_LGUI,                    KC_RALT,KC_DOWN,KC_UP,  KC_LEFT,OSL(MDIA),OSL(SYMB),
                                                                KC_TAB, KC_HOME, KC_END, KC_PGDN, KC_PGUP, KC_SPC
     ),
@@ -57,20 +57,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [SYMB] = LAYOUT_moonlander(
-    VRSN,   KC_F1,     KC_F2,      KC_F3,      KC_F4,    KC_F5,    KC_ESC,      _______,KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    _______,KC_EXLM,   KC_AT,      KC_LCBR,    KC_RCBR,  KC_PIPE,  _______,     _______,KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-    _______,KC_HASH,   KC_DLR,     KC_LPRN,    KC_RPRN,  KC_GRV,   _______,     _______,KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, _______,
-    _______,CTL_T(KC_PERC), ALT_T(KC_CIRC),KC_LBRC,KC_RBRC,KC_TILD,                     KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, _______,
-    _______,_______,_______,KC_LEFT,KC_RIGHT,  RGB_MOD,                                 RGB_TOG, KC_0,   KC_COMM, KC_DOT,  KC_EQL,  _______,
+        VRSN,   KC_F1,     KC_F2,      KC_F3,      KC_F4,    KC_F5,    KC_ESC,      _______,KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        _______,KC_EXLM,   KC_AT,      KC_LCBR,    KC_RCBR,  KC_PIPE,  _______,     _______,KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
+        _______,KC_HASH,   KC_DLR,     KC_LPRN,    KC_RPRN,  KC_GRV,   _______,     _______,KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, _______,
+        _______,CTL_T(KC_PERC), ALT_T(KC_CIRC),KC_LBRC,KC_RBRC,KC_TILD,                     KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, _______,
+        _______,_______,_______,KC_LEFT,KC_RIGHT,  RGB_MOD,                                 RGB_TOG, KC_0,   KC_COMM, KC_DOT,  KC_EQL,  _______,
                                                RGB_HUI,  RGB_VAI,   RGB_SPI,     TOGGLE_LAYER_COLOR,_______, _______
     ),
 
     [MDIA] = LAYOUT_moonlander(
-    EPRM,    _______, _______, _______, _______, _______, RESET,        KC_PSCR, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, KC_MS_U, _______, _______, _______,      _______, _______, KC_HOME, KC_UP,   KC_PGUP, _______, _______,
-    _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,      _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, KC_MPLY,
-    _______, _______, _______, _______, _______, _______,                        _______, KC_END,  _______, KC_PGDN, _______, _______,
-    _______, _______, _______, KC_BTN1, KC_BTN2, _______,                        _______, KC_VOLU, KC_VOLD, KC_MUTE, KC_MPRV, KC_MNXT,
+        EPRM,    _______, _______, _______, _______, _______, RESET,        KC_PSCR, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_MS_U, _______, _______, _______,      _______, _______, KC_HOME, KC_UP,   KC_PGUP, _______, _______,
+        _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,      _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, KC_MPLY,
+        _______, _______, _______, _______, _______, _______,                        _______, KC_END,  _______, KC_PGDN, _______, _______,
+        _______, _______, _______, KC_BTN1, KC_BTN2, _______,                        _______, KC_VOLU, KC_VOLD, KC_MUTE, KC_MPRV, KC_MNXT,
                                         _______, _______, _______,      _______, _______, _______
     ),
 };
@@ -82,10 +82,22 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         RGB_MATRIX_INDICATOR_SET_COLOR(12, 0xFF, 0x00, 0x00);  // S
         RGB_MATRIX_INDICATOR_SET_COLOR(17, 0xFF, 0x00, 0x00);  // D
         RGB_MATRIX_INDICATOR_SET_COLOR(22, 0xFF, 0x00, 0x00);  // F
-        RGB_MATRIX_INDICATOR_SET_COLOR(32, 0xFF, 0x00, 0x00);  // F
-        RGB_MATRIX_INDICATOR_SET_COLOR(33, 0xFF, 0x00, 0x00);  // F
-        RGB_MATRIX_INDICATOR_SET_COLOR(34, 0xFF, 0x00, 0x00);  // F
-        RGB_MATRIX_INDICATOR_SET_COLOR(35, 0xFF, 0x00, 0x00);  // F
+        RGB_MATRIX_INDICATOR_SET_COLOR(32, 0xFF, 0x00, 0x00);  // Left thumb cluster
+        RGB_MATRIX_INDICATOR_SET_COLOR(33, 0xFF, 0x00, 0x00);  //
+        RGB_MATRIX_INDICATOR_SET_COLOR(34, 0xFF, 0x00, 0x00);  //
+        RGB_MATRIX_INDICATOR_SET_COLOR(35, 0xFF, 0x00, 0x00);  //
+    }
+    if (layer_state_is(SYMB)) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(5,  0xFF, 0x00, 0x7F);  // 1
+        RGB_MATRIX_INDICATOR_SET_COLOR(10, 0xFF, 0x00, 0x7F);  // 2
+        RGB_MATRIX_INDICATOR_SET_COLOR(15, 0xFF, 0x00, 0x7F);  // 3
+        RGB_MATRIX_INDICATOR_SET_COLOR(20, 0xFF, 0x00, 0x7F);  // 4
+        RGB_MATRIX_INDICATOR_SET_COLOR(25, 0xFF, 0x00, 0x7F);  // 5
+        RGB_MATRIX_INDICATOR_SET_COLOR(41, 0xFF, 0x00, 0x7F);  // 0
+        RGB_MATRIX_INDICATOR_SET_COLOR(46, 0xFF, 0x00, 0x7F);  // 9
+        RGB_MATRIX_INDICATOR_SET_COLOR(51, 0xFF, 0x00, 0x7F);  // 8
+        RGB_MATRIX_INDICATOR_SET_COLOR(56, 0xFF, 0x00, 0x7F);  // 7
+        RGB_MATRIX_INDICATOR_SET_COLOR(61, 0xFF, 0x00, 0x7F);  // 6
     }
 
     // if (userspace_config.rgb_layer_change) {
@@ -172,25 +184,25 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
             eeconfig_init();
         }
         break;
-  }
-  return MACRO_NONE;
+    }
+    return MACRO_NONE;
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    // dynamically generate these.
+    switch (keycode) {
+        // dynamically generate these.
     case EPRM:
-      if (record->event.pressed) {
-        eeconfig_init();
-      }
-      return false;
-      break;
+        if (record->event.pressed) {
+            eeconfig_init();
+        }
+        return false;
+        break;
     case VRSN:
-      if (record->event.pressed) {
-        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-      }
-      return false;
-      break;
-  }
-  return true;
+        if (record->event.pressed) {
+            SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        }
+        return false;
+        break;
+    }
+    return true;
 }
